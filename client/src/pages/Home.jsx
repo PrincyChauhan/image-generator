f2159de377865f2f3fa10974305c94263d036084
PrincyChauhan <chauhanprincee7@gmail.com>
2024-08-30 11:55:07 +0530

    update home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,11 +2,53 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
+  padding: 30px 30px;
+  padding-bottom: 200px;
   height: 100%;
   overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  @media (max-width: 768px) {
+    padding: 6px 10px;
+  }
+  background: ${({ theme }) => theme.background};
+`;
+
+const HeadLine = styled.div`
+  font-size: 34px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_primary};
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Span = styled.div`
+  font-size: 30px;
+  font-weight: 800;
+  color: ${({ theme }) => theme.secondary};
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 1400px;
+  padding: 32px 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Home = () => {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <HeadLine>
+        Welcome to Popular Posts!
+        <Span>Generated with AI</Span>
+      </HeadLine>
+      <Wrapper></Wrapper>
+    </Container>
+  );
 };
 export default Home;
